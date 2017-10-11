c947910203243c0fb9899fc35ed7540de2958db5
Ben Holland <bhollan5@kent.edu>
2017-10-11 19:42:40 +0000

    Committing basic changes

--- benh/src/router/index.js
+++ benh/src/router/index.js
@@ -1,7 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from '@/components/Home'
-import admin from '@/components/Admin'
+import Hello from '@/components/Hello'
 
 Vue.use(Router)
 
@@ -9,13 +8,8 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'Home',
-      component: Home
-    },
-    {
-      path: '/admin',
-      name: 'admin',
-      component: admin
+      name: 'Hello',
+      component: Hello
     }
   ]
 })
